f343806dc426e08cd2b63ed62dd4bfc46948b1fb
Abdul Rahman Farzat <A.Farzat@protonmail.com>
2021-04-26 12:16:44 +0900

    Add some basic files such as zshrc, aliases, etc
    The zshrc should be soft-linked to the home directory as ~/.zshrc.

---
+++ aliases.zsh
@@ -0,0 +1,57 @@
+# Creates the given directories and then moves to the first one.
+alias mkcd='function _ { mkdir -p "${@}"; cd "${1}"; }; _'
+# Copy to clipboard.
+alias copy='xclip -sel clip'
+# Copy the current directory to the clipboard.
+alias pcopy='copy_buffer=$(pwd); copyechobuffer'
+# Uses the passCoder app and redirects the output to the clipboard.
+alias getp='function _ { copy_buffer=$(/home/farzat/files/Code/Projects/passCoder/passCoder.py "${@}"); echo -n $copy_buffer | copy; copy_buffer=""; }; _'
+# Switch from the current directory to the equivalent directory on another drive.
+alias sd='function _ { wd=$(pwd); wd="${wd/"${1}"/"${2}"}"; echo ${wd}; cd "${wd}"; }; _'
+# Edit bashrc.
+alias mbashrc='vim ~/.bashrc'
+# Edit bash history.
+alias mhistory='vim ~/.bash_history'
+# Edit bash aliases.
+alias maliasesload='source ~/.oh-my-zsh/custom/aliases.zsh'
+# Edit zshrc.
+alias mzshrc='vim -S ~/.oh-my-zsh/custom/Session.vim'
+# Edit vim configuration files.
+alias mvimrc='vim -S ~/.vim/vim.config/Session.vim'
+# Copy ISTU username to clipboard.
+alias copyechobuffer='echo -n $copy_buffer | copy; echo $copy_buffer'
+alias getsola='copy_buffer=solacom.intern@gmail.com; copyechobuffer'
+# Download using DownloadMedia projects.
+alias dyoutube='/home/farzat/files/Code/Projects/DownloadMedia/YouTube.sh'
+alias dudemy='/home/farzat/files/Code/Projects/DownloadMedia/Udemy.sh'
+alias dudemy2='/home/farzat/files/Code/Projects/DownloadMedia/Udemy2.sh'
+alias dudemy3='/home/farzat/files/Code/Projects/DownloadMedia/Udemy3.sh'
+alias dudemy4='/home/farzat/files/Code/Projects/DownloadMedia/Udemy4.sh'
+alias dyoutubed='dyoutube -d /home/farzat/files/Downloads/'
+# Get passwords.
+alias getpgithub='getp GitHub 1 72 Farzat.UG@outlook.jp A2 ""'
+alias getpfum='getp Microsoft 1 15 Farzat.uni@outlook.com C3 ""'
+alias getpudemy='getp Udemy 1 0 Farzat@outlook.com B2 ""'
+# Go to directories.
+alias cdmedia='echo /media/farzat; cd /media/farzat/'
+alias cdfiles='echo ~/files; cd ~/files/'
+alias cdwork='cd /home/farzat/files/Code/Projects/Work/Sola.com'
+alias cdvim='vim `git ls-files`'
+alias cdnode='cd /home/farzat/files/Code/Practices/Node/react/http-ajax; vim `find ! -path "./node_modules/*" ! -path "./build/*" -type f`'
+alias cdplayer='cd ~/files/Code/Projects/Media/media-player'
+alias cdserver='cd ~/files/Code/Projects/Media/media-server'
+# Create directories.
+alias mkdirpython='function _ { mkdir -p "${@}"; for dir in "${@}"; do touch "${dir}/__init__.py"; done; }; _'
+# Setting up servers.
+alias setupservermedia='http-server / -p 7700'
+# Suspend the machine.
+alias ssuspend='systemctl suspend'
+# Run authpass.
+alias authpass='~/files/Code/authpass/authpass'
+# Restore cursor to original colour and transparent behaviour before wal.
+alias restore_cursor="printf '%b' '\e]12'"
+# Find swap files.
+alias findswp='find / -type d \( -path /run/timeshift -o -path /media/farzat/Backup/timeshift \) -prune -false -o -name *.swp'
+# Set the background transparency in gnome-terminal.
+alias setTransperent='dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-transparent-background'
+alias setTransperency='dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/background-transparency-percent'
---
+++ general.zsh
@@ -0,0 +1,27 @@
+export MANPAGER='nvim +Man!'
+export PIPENV_VENV_IN_PROJECT=1
+export VISIUAL="/usr/bin/nvim"
+export EDITOR="/usr/bin/nvim"
+
+# Don't set the theme in tmux - it already inherits and the cursor colour there
+# can sometimes be a bit harder to restore.
+#[ -z "${TMUX}" ] && {
+    # Don't automatically run the sequence for all terminals as the cursor
+    # colour gets changed - instead skip for now and do it manually later on
+    # for the current terminal only.
+    wal --theme vscode -sq;
+    # Append a sequence which restores the cursor colour - doens't always work.
+    #printf '%s' ']12\' >> ~/.cache/wal/sequences;
+    # Run the sequences in the current terminal to apply the theme.
+    # The first is synchronous, the second is asynchronous.
+    cat ~/.cache/wal/sequences;
+    # (cat ~/.cache/wal/sequences &);
+    # To add support for TTYs this line can be optionally added.
+    source ~/.cache/wal/colors-tty.sh
+    # The following command attempts to restore the cursor colour.
+    printf '%b' '\e]12'
+# }
+
+export GOPATH="$HOME/.code_stuff/go"
+
+export PATH="${PATH}:${HOME}/.local/bin/"
