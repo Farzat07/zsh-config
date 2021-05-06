80f08e39ab28b490ee4ce6abf4b709da9d93094e
Abdul Rahman Farzat <A.Farzat@protonmail.com>
2021-05-06 15:49:11 +0900

    Add an alias for the default editor

--- aliases.zsh
+++ aliases.zsh
@@ -6,6 +6,8 @@ alias pcopy='copyechobuffer $(pwd)'
 alias mbashhistory='$EDITOR ~/.bash_history'
 # Edit zsh history.
 alias mhistory='$EDITOR ~/.zsh_history'
+# Edit using $EDITOR.
+alias edit='$EDITOR'
 # Edit zshrc.
 alias mzshrc='$EDITOR -S ~/.dotfiles/zsh/custom/Session.vim'
 # Edit $EDITOR configuration files.
