5285cf4cb07f288b1e82a5324f9a733234b37100
A Farzat <a@farzat.xyz>
2024-04-23 03:22:41 +0900

    Set history options

---
+++ halfway/options.zsh
@@ -0,0 +1,6 @@
+setopt extended_history  # Record timestamp of command in HISTFILE.
+setopt hist_expire_dups_first  # Delete duplicates first when HISTFILE size exceeds HISTSIZE.
+setopt hist_ignore_dups  # Ignore duplicated commands history list.
+setopt hist_ignore_space  # Ignore commands that start with space.
+setopt hist_verify  # Show command with history expansion to user before running it.
+setopt share_history  # Share command history data.
