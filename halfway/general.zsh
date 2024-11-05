60a5e909d480f5a69521401f4d151331dc4ba33f
A Farzat <a@farzat.xyz>
2024-11-05 15:06:59 +0900

    Only apply wal colours if files exist

--- halfway/general.zsh
+++ halfway/general.zsh
@@ -1,9 +1,9 @@
 # Run the sequences in the current terminal to apply the theme.
 # The first is synchronous, the second is asynchronous.
-cat ~/.cache/wal/sequences;
+[ -f "$XDG_CACHE_HOME/wal/sequences" ] && cat "$XDG_CACHE_HOME/wal/sequences"
 # (cat ~/.cache/wal/sequences &);
 # To add support for TTYs this line can be optionally added.
-source ~/.cache/wal/colors-tty.sh
+[ -f "$XDG_CACHE_HOME/wal/colors-tty.sh" ] && source "$XDG_CACHE_HOME/wal/colors-tty.sh"
 # Attempts to restore the cursor colour - harmless, usually doesn't work
 # but sometimes it is actually the only thing that works.
 printf '%b' '\e]12'
